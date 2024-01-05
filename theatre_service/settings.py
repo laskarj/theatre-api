e499dfaf92633914877972d3282babc60353f519
laskarj <koks.adekvat@gmail.com>
2024-01-05 17:11:48 +0100

    Create User Custome Manager and User model, connect User app to settings and make the first migrations, add AUTH_USER_MODEL

--- theatre_service/settings.py
+++ theatre_service/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+
+    # Apps
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -99,6 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "user.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
@@ -109,7 +114,7 @@ TIME_ZONE = "UTC"
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
