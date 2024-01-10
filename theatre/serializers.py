a69731338cb917482dff5621f7a338043b428179
laskarj <koks.adekvat@gmail.com>
2024-01-10 19:06:58 +0100

    Add Serializers and views: - Create Performance View witch allow POST, RETRIEVE, list - add image for Performance model - Create Serializers Performance(list and detail), TicketSeats wich show row and seat wich were taken to the Performance, TheatreHallSerializer - Create annotate field for Performance view "tickets_available"

--- theatre/serializers.py
+++ theatre/serializers.py
@@ -4,6 +4,9 @@ from theatre.models import (
     Artist,
     Genre,
     Play,
+    Performance,
+    Ticket,
+    TheatreHall,
 )
 
 
@@ -27,6 +30,7 @@ class ArtistSerializer(serializers.ModelSerializer):
             "about",
             "image",
         )
+
 
 class ArtistListSerializer(serializers.ModelSerializer):
     class Meta:
@@ -112,3 +116,68 @@ class ImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Artist
         fields = ("id", "image", )
+
+
+class PerformanceSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Performance
+        fields = (
+            "id",
+            "theatre_hall",
+            "show_time",
+            "image",
+            "play"
+        )
+
+
+class PerformanceListSerializer(serializers.ModelSerializer):
+    plays_title = serializers.CharField(source="play.title", read_only=True)
+    theatre_hall_name = serializers.CharField(
+        source="theatre_hall.name", read_only=True
+    )
+    theatre_hall_capacity = serializers.IntegerField(read_only=True)
+    tickets_available = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = Performance
+        fields = (
+            "id",
+            "show_time",
+            "plays_title",
+            "image",
+            "theatre_hall_name",
+            "theatre_hall_capacity",
+            "tickets_available",
+        )
+
+
+class TheatreHallSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TheatreHall
+        fields = ("id", "name", "rows", "seats_in_row", )
+
+
+class TicketSeatsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ticket
+        fields = ("row", "seat", )
+
+
+class PerformanceDetailSerializer(serializers.ModelSerializer):
+    taken_places = TicketSeatsSerializer(
+        source="tickets", many=True, read_only=True
+    )
+    theatre_hall = TheatreHallSerializer(many=False, read_only=True)
+    play = PlayListSerializer(many=False, read_only=True)
+
+    class Meta:
+        model = Performance
+        fields = (
+            "id",
+            "image",
+            "show_time",
+            "play",
+            "theatre_hall",
+            "taken_places",
+        )
--- theatre/urls.py
+++ theatre/urls.py
@@ -5,13 +5,14 @@ from theatre.views import (
     ArtistViewSet,
     GenreViewSet,
     PlayViewSet,
+    PerformanceViewSet,
 )
 
 router = routers.DefaultRouter()
 router.register("genres", GenreViewSet)
 router.register("artists", ArtistViewSet)
 router.register("plays", PlayViewSet)
-
+router.register("performances", PerformanceViewSet)
 
 urlpatterns = [path("", include(router.urls))]
 
