d313d36398b9505eabb6ad2312efa9aaee4289ef
laskarj <koks.adekvat@gmail.com>
2024-01-12 14:49:17 +0100

    Add permissions for theatre app - GenreViewSet, ArtistViewSet, PlayViewSet, PerformanceViewSet < allow GET and POST for the admin user, but only GET for all other users; - ReservationViewSet < available only authenticated user for GET and POST actions.

---
+++ theatre/permissions.py
@@ -0,0 +1,17 @@
+from rest_framework.permissions import SAFE_METHODS, BasePermission
+from rest_framework.request import Request
+from rest_framework.generics import GenericAPIView
+
+class IsAdminUserOrReadOnly(BasePermission):
+
+    def has_permission(
+            self,
+            request: Request,
+            view: GenericAPIView
+    ) -> bool:
+        return bool(
+            request.method in SAFE_METHODS
+            or (
+                    request.user and request.user.is_staff
+            )
+        )
