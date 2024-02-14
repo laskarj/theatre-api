fb263092514669bdf90df3277741baa38a68cd8c
laskarj <koks.adekvat@gmail.com>
2024-02-14 11:10:38 +0100

    Added tests for Artists filtering: - by full_name, last_name or first_name.

--- theatre/tests/test_artists_api.py
+++ theatre/tests/test_artists_api.py
@@ -8,7 +8,7 @@ from theatre.models import Artist
 from theatre.serializers import ArtistListSerializer
 
 
-ARTISTS_BASE_API = reverse("theatre:artist-list")
+ARTISTS_BASE_URL = reverse("theatre:artist-list")
 
 
 def sample_artist(**params) -> Artist:
@@ -29,10 +29,55 @@ class UnauthenticatedArtistsApiTests(TestCase):
         sample_artist()
         sample_artist()
 
-        response = self.client.get(ARTISTS_BASE_API, data={"page": 1})
+        response = self.client.get(ARTISTS_BASE_URL, data={"page": 1})
 
         artists = Artist.objects.all()
         serializer = ArtistListSerializer(artists, many=True)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["results"], serializer.data)
+
+    def test_artists_filtering_by_full_name(self):
+        artist1 = sample_artist(first_name="First1", last_name="Last1")
+        artist2 = sample_artist(first_name="First2", last_name="Last2")
+
+        response = self.client.get(
+            ARTISTS_BASE_URL,
+            {"search_by": f"{artist1.first_name} {artist1.last_name}"},
+        )
+
+        serializer1 = ArtistListSerializer(artist1)
+        serializer2 = ArtistListSerializer(artist2)
+
+        self.assertIn(serializer1.data, response.data["results"])
+        self.assertNotIn(serializer2.data, response.data["results"])
+
+    def test_artists_filtering_by_first_name(self):
+        artist1 = sample_artist(first_name="First1", last_name="Last1")
+        artist2 = sample_artist(first_name="First2", last_name="Last2")
+
+        response = self.client.get(
+            ARTISTS_BASE_URL,
+            {"search_by": artist1.first_name},
+        )
+
+        serializer1 = ArtistListSerializer(artist1)
+        serializer2 = ArtistListSerializer(artist2)
+
+        self.assertIn(serializer1.data, response.data["results"])
+        self.assertNotIn(serializer2.data, response.data["results"])
+
+    def test_artists_filtering_by_last_name(self):
+        artist1 = sample_artist(first_name="First1", last_name="Last1")
+        artist2 = sample_artist(first_name="First2", last_name="Last2")
+
+        response = self.client.get(
+            ARTISTS_BASE_URL,
+            {"search_by": artist1.last_name},
+        )
+
+        serializer1 = ArtistListSerializer(artist1)
+        serializer2 = ArtistListSerializer(artist2)
+
+        self.assertIn(serializer1.data, response.data["results"])
+        self.assertNotIn(serializer2.data, response.data["results"])
